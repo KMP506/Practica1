68a6085a7739a71dccd37e8cbd3632ca15a3b0e1
Chiesaac7 <isoportosa@est.utn.ac.cr>
2026-06-29 22:09:23 -0600

    Futbol, Baloncesto y Volei

---
+++ src/main/java/disciplinas/Baloncesto.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package disciplinas;
+
+/**
+ *
+ * @author isaac
+ */
+public class Baloncesto extends Categoria{
+    
+    public Baloncesto(String nombre, String categoria, int capacidadMaxima) {
+        super(nombre, categoria, capacidadMaxima);
+    }
+    
+ 
+}
---
+++ src/main/java/disciplinas/Voleibol.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package disciplinas;
+
+/**
+ *
+ * @author isaac
+ */
+public class Voleibol extends Categoria {
+    
+    public Voleibol(String nombre, String categoria, int capacidadMaxima) {
+        super(nombre, categoria, capacidadMaxima);
+    }
+    
+}
