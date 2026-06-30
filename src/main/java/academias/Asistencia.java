ea1ded53cc3216e6c3bce3340c187620c65f8a96
Efrain Fernandez <effernandezcr@est.utn.ac.cr>
2026-06-29 22:26:31 -0600

    asistencia
    se crea la clase asistencia

--- src/main/java/academias/Asistencia.java
+++ src/main/java/academias/Asistencia.java
@@ -4,10 +4,43 @@
  */
 package academias;
 
+import personas.Participante;
+
 /**
  *
  * @author kevin
  */
 public class Asistencia {
+    private Participante participante; 
+    private Entrenamiento entrenamiento;
+    private boolean asistio;
+
+    public Asistencia(Participante participante, Entrenamiento entrenamiento, boolean asistio) {
+        this.participante = participante;
+        this.entrenamiento = entrenamiento;
+        this.asistio = asistio;
+    }
+
+    public Participante getParticipante() {
+        return participante;
+    }
+
+    public Entrenamiento getEntrenamiento() {
+        return entrenamiento;
+    }
+
+    public void setParticipante(Participante participante) {
+        this.participante = participante;
+    }
+
+    public void setEntrenamiento(Entrenamiento entrenamiento) {
+        this.entrenamiento = entrenamiento;
+    }
+
+    public void setAsistio(boolean asistio) {
+        this.asistio = asistio;
+    }
+    
+    
     
 }
